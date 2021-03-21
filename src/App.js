0b77f937f965e27f6d88e6a8d9679731d57d7f97
Tina Skov Olesen <tinaskovo@gmail.com>
2021-03-21 21:02:53 +0100

    undoing delete

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Politiken from './politiken'
+import Portfolio from './portfolio'
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route path="/" exact component={() => <Portfolio />} />
         <Route path="/politiken" exact component={() => <Politiken />} />
       </Switch>
     </Router>
-
-    // <div className="portfolio-container">
-    //   <div className="landing-page">
-    //     <h1>Hi, I am Developer</h1>
-    //     <button>See my work</button>
-    //   </div>
-    //   <div className="work">
-    //     <h1>This is my work</h1>
-    //     <div>Interacoustics</div>
-    //     <div>Medical</div>
-    //     <div>Le Wagon</div>
-    //     <button>About</button>
-    //   </div>
-    //   <div className="about">
-    //     <h1>About</h1>
-    //     <p>I am great developer</p>
-    //     <button>Contact</button>
-    //   </div>
-    //   <div className="contact">
-    //     <h2>Call me</h2>
-    //     <p>+45 666</p>
-    //     <button>To top</button>
-    //   </div>
-    // </div>
-    // Navbar
-    // Scrolltotop
-    // Footer
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index";
-import App from "./app";
+import App from "./app.js";
 
 ReactDOM.render(
   <React.StrictMode>
