2e6248c15a8bbeca0f2e99dd55fbc991a5777edd
Tina Skov Olesen <tinaskovo@gmail.com>
2021-03-22 19:46:36 +0100

    remove portfolio files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Politiken from './politiken'
-import Portfolio from './portfolio'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={() => <Portfolio />} />
         <Route path="/politiken" exact component={() => <Politiken />} />
       </Switch>
     </Router>
